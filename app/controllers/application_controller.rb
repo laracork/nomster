d15a36a5033f680b8380cd70dbcb5594ab6ab2ff
Andrew Max <anm898989@gmail.com>
2018-03-15 18:28:22 +0000

    andrews feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # hey it's a comment from andrew
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def andrews_method
+    puts "hello its andrew"
+  end
 end
