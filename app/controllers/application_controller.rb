9f176347fde45b52d8ab6176badc070c6c0f89d6
Lara Corkrey <laracorkrey@gmail.com>
2018-03-17 01:20:08 +0000

    added photo form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
-# hey it's a comment from andrew
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def andrews_method
-    puts "hello its andrew"
-  end
-end
