28c343734379055afda680b1ecf0d8c704fcf5b8
Lara Corkrey <laracorkrey@gmail.com>
2018-03-24 02:02:58 +0000

    updated code after reattaching head

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,5 +7,5 @@ class Place < ApplicationRecord
   after_validation :geocode
 
   validates :name, length: {minimum: 3}, presence: true 
-  validates :address, :description, :photos, presence: true
+  validates :address, :description, presence: true
 end
