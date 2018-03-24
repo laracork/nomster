5c552960d72b5e2b7f7c2c12219c1824db60e5aa
Lara Corkrey <laracorkrey@gmail.com>
2018-03-24 02:02:58 +0000

    updated code after reattaching head

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,7 +10,6 @@ class PlacesController < ApplicationController
   end
 
   def create
-    binding.pry
     @place = current_user.places.create(place_params)
     if @place.valid?
       redirect_to root_path
